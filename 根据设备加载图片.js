cb9ec6b9ab962342d3189cfc41eb6eaa3eb4eecb
xiaoxuenice <50929483+xiaoxuenice@users.noreply.github.com>
2021-12-30 18:47:57 +0800

    Update 根据设备加载图片.js

--- 根据设备加载图片.js
+++ 根据设备加载图片.js
@@ -17,23 +17,6 @@ $(function(){
 </script>
 
 <img  id="goBtn"  class="pic-html" />
-        
-        
-<script>
-        var webArr = ['https://taobao.com']
-        function RandomNum(Min, Max) {
-            var Range = Max - Min;
-            var Rand = Math.random();
-            var num = Min + Math.floor(Rand * Range);
-            return num;
-        }
-
-        var webLength = webArr.length;
-        var mathNum = RandomNum(0, webLength);
-        $('#goBtn').click(function () {
-            window.location.href = webArr[mathNum];
-        })
-</script>
 
 
 ------------------------------------------------------------------------------------------------------------------
