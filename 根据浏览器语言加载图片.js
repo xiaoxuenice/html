6352a378d1f93c039045531a2a55365625bf4e1d
xiaoxuenice <50929483+xiaoxuenice@users.noreply.github.com>
2022-01-05 17:37:15 +0800

    Create 根据浏览器语言加载图片.js

---
+++ 根据浏览器语言加载图片.js
@@ -0,0 +1,19 @@
+<script language="JavaScript">
+var jsSrc =(navigator.language || navigator.browserLanguage).toLowerCase();
+if(jsSrc.indexOf('zh')!=-1)
+{
+document.write('<img id="gocn" src="/images/cn.gif">');
+}
+else if(jsSrc.indexOf('vi')!=-1)
+{
+document.write('<img id="goyn" src="/images/yn.gif">');
+}
+</script>
+
+
+
+ <script>
+        $('#goyn').click(function () {
+            window.location.href = 'https://jdyn.com';
+        })      
+</script>
