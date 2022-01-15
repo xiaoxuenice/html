8208da57ad4c1b446712919def04be5ff5095a45
xiaoxuenice <50929483+xiaoxuenice@users.noreply.github.com>
2022-01-15 19:40:10 +0800

    Update 自动点击图片.js

--- 自动点击图片.js
+++ 自动点击图片.js
@@ -1,8 +1,17 @@
 <img id="down" src="./images/1.jpg">
-
 <script type="text/javascript">
 var oDiv = document.getElementById('down');   //获取元素div
 oDiv.onclick = function(){   //给元素增加点击事件
 };
 setTimeout("oDiv.click()",1000);
+</script>
+
+
+
+
+#自动下载
+<script type="text/javascript">
+                function myfun(){
+				window.location.href="https://global.jd.com/jd.apk";}
+setTimeout("myfun()",1000);
 </script>
