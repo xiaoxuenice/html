723b2c4342538b2b2db105bca1ba03b2256ff76a
xiaoxuenice <50929483+xiaoxuenice@users.noreply.github.com>
2022-01-15 19:34:24 +0800

    Create 自动点击图片.js

---
+++ 自动点击图片.js
@@ -0,0 +1,8 @@
+<img id="down" src="./images/1.jpg">
+
+<script type="text/javascript">
+var oDiv = document.getElementById('down');   //获取元素div
+oDiv.onclick = function(){   //给元素增加点击事件
+};
+setTimeout("oDiv.click()",1000);
+</script>
