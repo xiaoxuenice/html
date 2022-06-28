d50f3b680215826460af9d1d74a64947253bdbf8
weidong <weidong@qq.com>
2022-06-28 23:14:42 +0800

    update

--- ajax/views.py
+++
@@ -1,5 +0,0 @@
-def test(request):
-    if request.method == "POST":
-        print(json.loads(request.body.decode()))
-        return HttpResponse(request.body)
-    return  render(request,'ajax.html')
